349b1cfa2276476fbfd1c89292746748ad721c33
CasandraCalderon <angie200896@gmail.com>
2022-03-24 08:48:42 -0400

    seguridad de la pagina

--- pages/navbar/navbar-top.php
+++ pages/navbar/navbar-top.php
@@ -13,6 +13,9 @@
       $_SESSION['id_user'] = $user['id'];
     }
   }
+  if (!isset($user)) {
+    header("Location: ../logout.php");  
+  }
 ?>
 <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light"> <button type="button" id="sidebarCollapse" class="btn btn-success"> <i class="fa fa-align-justify"></i> </button> <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
